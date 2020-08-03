c5754ae51e50751cc192e7646db73bb6053d3c4d
oimq <taep0q@gmail.com>
2020-08-03 22:23:28 +0900

    Modifying setup.py

--- det2Clo/__init__.py
+++
@@ -1,3 +0,0 @@
-from .Det2 import Det2
-from .cloDet import cloDet
-from .visIm import visIm
--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(name='det2Clo',
-      version=2.0,
+      version='3.0',
       url='https://github.com/oimq/det2Clo',
       author='oimq',
       author_email='taep0q@gmail.com',
       description='Image categorization and segmentation based on detectron2',
       packages=find_packages(),
-      install_requires=['numpy', 'opencv-python', 'tqdm', 'pytorch', 'pillow', 'detectron2'],
+      install_requires=[
+            'numpy==1.19.0', 'opencv-python==4.2.0.34', 'tqdm',
+            'torch==1.4.0', 'torchvision==0.5.0', 'Pillow==7.1.2'
+      ],
       zip_safe=False
 )
